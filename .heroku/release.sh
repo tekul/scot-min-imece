e70d8cd0cd23e1cba981d6ddc781d6b0ba907b14
Luke Taylor <tekul@pm.me>
2021-10-08 23:07:34 +0100

    Fix variable escaping in release.sh

--- .heroku/release.sh
+++ .heroku/release.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-echo "******* Running release script *******"
-
-npm --prefix server run recreate-db:prod
-
 [ -z "$GITHUB_REPO" ] \
     && ( echo "GITHUB_REPO is not set!"; exit 1 )
 
 [ -z "$GITHUB_TOKEN" ] \
     && ( echo "GITHUB_TOKEN is not set!"; exit 1 )
+
+npm --prefix server run recreate-db:prod
 
 repo=$GITHUB_REPO
 token=$GITHUB_TOKEN
@@ -18,15 +16,22 @@ token=$GITHUB_TOKEN
 app=$HEROKU_APP_NAME
 pr=$HEROKU_PR_NUMBER
 
-curl -X POST \
-     -H "Accept: application/vnd.github.v3+json" \
-     -H "Authorization: Bearer $token" \
-     https://api.github.com/repos/$repo/dispatches \
-     -d '{"event_type":"review-app-test",
-          "client_payload":{
-          "review_app_url": "https://$app.herokuapp.com",
-          "PR_NUM": "$pr"
-         }
-        }'
+post_data()
+{
+  cat <<EOF
+{
+  "event_type":"review-app-test",
+  "client_payload":{
+    "review_app_url": "https://${app}.herokuapp.com",
+    "PR_NUM": "${pr}"
+  }
+}
+EOF
+}
+
+curl -i \
+-H "Accept: application/vnd.github.v3+json" \
+-H "Authorization: Bearer $token" \
+--data "$(post_data)" "https://api.github.com/repos/$repo/dispatches"
 
 echo "Done."
