e6a0ca680acb6b16d47959e70e09850ea1938d9c
Luke Taylor <tekul@pm.me>
2021-10-08 22:42:37 +0100

    Remove blank lines in release.sh

--- .heroku/release.sh
+++ .heroku/release.sh
@@ -19,17 +19,14 @@ app=$HEROKU_APP_NAME
 pr=$HEROKU_PR_NUMBER
 
 curl -X POST \
-
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer $token" \
-
      https://api.github.com/repos/$repo/dispatches \
-
      -d '{"event_type":"review-app-test",
-       "client_payload":{
-        "review_app_url": "https://$app.herokuapp.com",
-        "PR_NUM": "$pr"
-       }
-      }'
+          "client_payload":{
+          "review_app_url": "https://$app.herokuapp.com",
+          "PR_NUM": "$pr"
+         }
+        }'
 
 echo "Done."
